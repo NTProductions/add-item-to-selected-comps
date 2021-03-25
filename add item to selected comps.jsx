3a209bffbcf555847c39130c1218d90222b4412b
Nate Lovell <Natelovell99@gmail.com>
2021-03-25 15:49:52 -0600

    Update add item to selected comps.jsx

--- add item to selected comps.jsx
+++ add item to selected comps.jsx
@@ -10,28 +10,24 @@ window.orientation = "column";
 
 var instructionsText = window.add("statictext", undefined, "Select compositions in 'Project' panel");
 
-var groupZero = window.add("group", undefined, "groupZero");
-groupZero.orientation = "row";
-var ddText = groupZero.add("statictext", undefined, "Item to add:");
-var layerDD = groupZero.add("dropdownlist", undefined, getItems());
-layerDD.selection = 0;
-
 var group = window.add("group", undefined, "group");
 group.orientation = "row";
 var openComps = group.add("checkbox", undefined, "Open Comps");
 var button = group.add("button", undefined, "Add+");
+
+getItems();
 
 window.center();
 window.show();
 
 button.onClick = function() {
         app.beginUndoGroup("Add Item Process");
-        main(items[layerDD.selection.index], openComps.value);
+        main(items, openComps.value);
         app.endUndoGroup();
         alert("Complete!");
     }
 
-function main(itemToAdd, openCompsBool) {
+function main(itemsToAdd, openCompsBool) {
         var compsToUse = [];
         for(var i = 1; i <= app.project.numItems; i++) {
             if(app.project.item(i).selected) {
@@ -43,7 +39,9 @@ function main(itemToAdd, openCompsBool) {
         
         var addedLayer;
         for(var i = 0; i < compsToUse.length; i++) {
-            addedLayer = compsToUse[i].layers.add(itemToAdd);
+            for(var e = 0; e < itemsToAdd.length; e++) {
+            addedLayer = compsToUse[i].layers.add(itemsToAdd[e]);
+            }
             if(openCompsBool == true) {
                     compsToUse[i].openInViewer();
                 }
@@ -55,11 +53,13 @@ function getItems() {
         itemNames = [];
 
         for(var i = 1; i <= app.project.numItems; i++) {
+            if(app.project.item(i).selected) {
             if(!(app.project.item(i) instanceof FolderItem)) {
                     items.push(app.project.item(i));
                     itemNames.push(app.project.item(i).name);
                 }
             }
+        }
         
         return itemNames;
     }
